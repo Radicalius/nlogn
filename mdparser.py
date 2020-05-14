2c79b58e556cb7a687edd2926d725ab1f157015a
Radicalius <mr.zacharycotton@gmail.com>
2020-05-13 22:56:37 -0700

    Article Lists, Styles, Markdown Parser, More Routes

---
+++ mdparser.py
@@ -0,0 +1,184 @@
+import re
+
+TOKENS = ["@", "\n+", "#", "##", "###", "_", "__", "-", "--", "---", "\\*\\*", "[\t ]+", "[\t ]*\\*", "~", "~~", "```", "`", "``", ">", "> ", "\\!", "\\!\\[", "\\[","\\]", "[a-zA-Z0-9 ,.!$%^&(){}=;'+:/]+"]
+
+def tokenize(md):
+    tokens = []
+    indentation = 0
+    newline = False
+    while len(md) > 0:
+        ahead = 1
+        last_matched = ""
+        while ahead < len(md):
+            matched = False
+            for i in TOKENS:
+                match = re.match(i, md[:ahead])
+                if match and match.end() == ahead:
+                    matched = True
+                    last_matched = i
+            if not matched:
+                break
+            else:
+                ahead += 1
+
+        if last_matched == "[\t ]+" and newline:
+            if ahead - 1 > indentation:
+                last_matched = "INDENT"
+                indentation = ahead-1
+            elif ahead - 1 < indentation:
+                last_matched = "DEDENT"
+                indentation = ahead-1
+            else:
+                md = md[max(1,ahead-1):]
+                ahead = 1
+                continue
+            newline = False
+
+        elif newline and indentation > 0:
+            print(last_matched)
+            indentation = 0
+            tokens.append(("DEDENT",""))
+            newline = False
+
+        if last_matched == "\n+":
+            newline = True
+
+        tokens.append((last_matched, md[:ahead-1]))
+        md = md[max(1,ahead-1):]
+        ahead = 1
+    return tokens
+
+def parse(tokens):
+    text = ""
+    if not tokens:
+        return ""
+    if tokens[0][0] == "":
+        return tokens.pop(0)[1]
+    elif tokens[0][0] == "[a-zA-Z0-9 ,.!$%^&(){}=;'+:/]+":
+        return tokens.pop(0)[1]
+    elif tokens[0][0] == "@":
+        text+=parse_tag(tokens)
+    elif tokens[0][0] in ["#", "##", "###"]:
+        text += parse_header(tokens)
+    elif tokens[0][0] == "\n+":
+        text += "<br/>"*len(tokens[0][1])
+        tokens.pop(0)
+    # elif tokens[0][0] == "[\t ]*\\*":
+    #     text += parse_list(tokens)
+    elif tokens[0][0] in ["\\*\\*", "--", "~~", "__"]:
+        text += parse_style(tokens)
+    elif tokens[0][0] == "> ":
+        text += parse_quote(tokens)
+    elif tokens[0][0] == "`":
+        text += parse_inline_code(tokens)
+    elif tokens[0][0] == "```":
+        text += parse_code_block(tokens)
+    elif tokens[0][0] == "\\[":
+        text += parse_link(tokens)
+    elif tokens[0][0] == "\\!\\[":
+        text += parse_image(tokens)
+    elif tokens[0][0] == "---":
+        text += "<hr/>\n"
+    else:
+        text += tokens.pop(0)[0]
+    return text
+
+def parse_style(tokens):
+    type = tokens.pop(0)[0]
+    styles = {"\\*\\*": "b", "--": "i", "~~": "s", "__":"u"}
+    text = "<{0}>".format(styles[type])
+    while tokens and tokens[0][0] != type:
+        text += parse(tokens)
+    if tokens:
+        tokens.pop(0)
+    text += "</{0}>".format(styles[type])
+    return text
+
+def parse_inline_code(tokens):
+    text = "<code>"
+    tokens.pop(0)
+    while tokens and tokens[0][0] != "`" and tokens[0][0] != "\n+":
+        text += tokens.pop(0)[1]
+    if tokens:
+        tokens.pop(0)
+    text += "</code>"
+    return text
+
+def parse_code_block(tokens):
+    text = "<pre>"
+    tokens.pop(0)
+    while tokens and tokens[0][0] != "```":
+        text += tokens.pop(0)[1]
+    if tokens:
+        tokens.pop(0)
+    text += "</pre>"
+    return text
+
+def parse_quote(tokens):
+    text = "<blockquote>"
+    while tokens and tokens[0][0] == "> ":
+        tokens.pop(0)
+        while tokens and tokens[0][0] != "\n+":
+            text += parse(tokens)
+        if len(tokens) >= 2 and tokens[1][0] == "> ":
+            text += parse(tokens)
+        else:
+            tokens.pop(0)
+    text += "</blockquote>"
+    return text
+
+def parse_header(tokens):
+    text = ""
+    heads = {"#": "h1", "##": "h2", "###": "h3"}
+    type = tokens.pop(0)[0]
+    text += "<{0}>".format(heads[type])
+    while tokens and tokens[0][0] != "\n+":
+        text += tokens[0][1]
+        tokens.pop(0)
+    tokens.pop(0)
+    text += "</{0}>\n".format(heads[type])
+    return text
+
+def parse_link(tokens):
+    if len(tokens) < 4:
+        return "["
+    href = tokens[3][1].split(")")[0][1:]
+    post_text = tokens[3][1].split(")")[1]
+    link_text = tokens[1][1]
+    for i in range(4):
+        tokens.pop(0)
+    return "<a href={0}>{1}</a> {2}".format(href, link_text, post_text)
+
+def parse_image(tokens):
+    if len(tokens) < 4:
+        return "!["
+    src = tokens[3][1][1:-1]
+    desc = tokens[1][1]
+    for i in range(4):
+        tokens.pop(0)
+    return "<br/><img src={0} desc={1} /><br/>".format(src, desc)
+
+def parse_tag(tokens):
+    tokens.pop(0)
+    print(tokens[0])
+    if tokens[0][0] == "[a-zA-Z0-9 ,.!$%^&(){}=;'+:/]+":
+        return "<tag>"+tokens.pop(0)[1].strip()+"</tag>&nbsp;"
+    return "@"
+
+def parse_list(tokens):
+    return ""
+
+
+def render_html(md_file):
+    md = open(md_file).read()
+    html = ""
+    tokens = tokenize(md)
+    print(tokens)
+    l = len(tokens)
+    while len(tokens):
+        html += parse(tokens)
+        if len(tokens) == l:
+            tokens.pop(0)
+        else:
+            l = len(tokens)
+    return html
--- server.py
+++ server.py
@@ -1,12 +1,29 @@
-import flask, os
+import flask, os, json
 from flask import Flask, render_template, send_file
+from mdparser import *
 
 template_dir = os.path.abspath("html")
 app = Flask(__name__, template_folder=template_dir)
 
+md = {}
+def caching_render_html(file):
+    if file in md:
+        return md[file]
+    else:
+        md[file] = render_html(file)
+        return md[file]
+
+mf = json.load(open("markdown/manifest.json"))
+
 @app.route("/")
 def index():
-    return render_template("base.html", page="index.html")
+    return render_template("base.html", markdown=False, page="index.html", posts=mf["files"])
+
+@app.route("/<t>")
+def computing(t):
+    if t not in ["music", "computing", "datascience"]:
+        return "404 Error: Page Not Found", 404
+    return render_template("base.html", type=t.capitalize(), markdown=False, page="index.html", posts=[i for i in mf["files"] if t in i["tags"]])
 
 @app.route("/articles/<name>")
 def article(name):
