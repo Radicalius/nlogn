d3342561c260eb9588a737cf3ed669951d70a7cb
Zachary Cotton <mr.zacharycotton@gmail.com>
2020-09-12 23:23:58 -0700

    Allow Uploads to Cache with Authorized Digital Signature

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM nginx
 COPY requirements.txt requirements.txt
 
 RUN apt-get update; \
-apt-get install -y cron curl python3 python3-pip; \
+apt-get install -y python3 python3-pip; \
 python3 -m pip install -r requirements.txt
 
 COPY proxy/nginx.conf /etc/nginx/nginx-template.conf
--- cache/server.py
+++ cache/server.py
@@ -1,8 +1,23 @@
 import flask
-from flask import Flask
+from flask import Flask, request
 import requests
 import sys, os
 import redis
+
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+import Crypto.Signature.pkcs1_15
+import base64
+
+def verify(publickey,data,sign):
+    hash = SHA256.new(data.encode("utf8"))
+    signer = Crypto.Signature.pkcs1_15.new(RSA.import_key(publickey))
+    try:
+        signer.verify(hash, base64.b64decode(sign))
+        return True
+    except ValueError:
+        return False
 
 app = Flask(__name__, template_folder=".")
 r = redis.Redis.from_url(os.environ["REDIS_URL"])
@@ -14,10 +29,16 @@ def get(domain, file):
         return "", 404
     return resp, 200
 
-@app.route("/<domain>/<file>", methods=["POST"])
+@app.route("/<domain>/<file>", methods=["POST", "PUT"])
 def post(domain, file):
-    cont = requests.get("https://"+domain+".nlogn.blog/"+file, timeout=2).text
-    r.set(domain+"/"+file, cont)
+    pub_key = r.get(domain+"/key.pub")
+    json = request.get_json()
+    if not verify(pub_key, json["content"], json["signature"]):
+        return "", 401
+    if request.method == "POST":
+        r.set(domain+"/"+file, json["content"])
+    else:
+        r.append(domain+"/"+file, json["content"])
     return "", 201
 
 if __name__ == "__main__":
